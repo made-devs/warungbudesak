3bc8713322783eab752671798c1cd7fc720c51bd
madedev <nmaderama@gmail.com>
2024-09-10 21:38:11 +0700

    Fix home img

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from 'react-redux';
 import CreateUser from '../features/user/CreateUser';
-import Button from './Button';
 import { Link } from 'react-router-dom';
 
 function Home() {
@@ -8,12 +7,12 @@ function Home() {
   return (
     <div className="transition-all">
       <img
-        src="../../public/full-art3.webp"
+        src="/full-art3.webp"
         className="absolute right-0 top-0 z-0 hidden xl:block"
       />
       <div className="mx-auto my-8 flex w-[60%] flex-col items-center text-center xl:ml-[1rem] xl:mt-[4rem] xl:w-[40%]">
         <img
-          src="../../public/mobile2.webp"
+          src="/mobile2.webp"
           className={`w-[70%] ${username ? 'my-10' : ''}`}
         />
 
@@ -31,15 +30,15 @@ function Home() {
         )}
       </div>
       <img
-        src="../../public/mobile1.webp"
+        src="/mobile1.webp"
         className="absolute left-0 z-0 w-[40rem] sm:hidden"
       />
       <img
-        src="../../public/full-art1.webp"
+        src="/full-art1.webp"
         className="absolute bottom-0 left-0 hidden sm:block sm:w-[45rem]"
       />
       <img
-        src="../../public/full-art2.webp"
+        src="/full-art2.webp"
         className="absolute bottom-0 right-0 hidden w-[50%] sm:block xl:w-[60%]"
       />
     </div>
