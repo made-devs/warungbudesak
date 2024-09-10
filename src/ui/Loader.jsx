cc06996e8c9645da8a5b64a09931eaa5b6e9461a
madedev <nmaderama@gmail.com>
2024-09-10 22:09:34 +0700

    Fix loader

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div className="absolute inset-0 z-10 flex items-center justify-center bg-slate-200/35 backdrop-blur-sm">
+    <div className="absolute inset-0 z-30 flex items-center justify-center bg-slate-200/35 backdrop-blur-sm">
       <div className="loader"></div>
     </div>
   );
